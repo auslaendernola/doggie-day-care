6e94d5ac51be05e02c4417cfd3a5421555bc1224
Tyler Black Hernandez <tylerblackhernandez@gmail.com>
2017-02-06 08:36:30 -0600

    Add Better Errors

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,24 @@
+class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+  def new
+    @dog = Dog.new
+  end
+  def create
+    @dog = Dog.new(dog_params)
+
+    if @dog.save
+      redirect_to dogs_url, notice: "Successfully Saved"
+    else
+      flash.now[:notice] = "Something went wrong, and we couldn't save your dog."
+      render :new
+    end
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :age, :breed)
+  end
+end
